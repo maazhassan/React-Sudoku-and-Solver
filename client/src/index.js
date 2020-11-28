dfab980c59e2cc4fb489760cc0379a602a04d17a
maaz.hassan <h.maaz809@hotmail.com>
2020-11-28 04:51:18 -0700

    basic board layout, click handler test

--- client/src/index.js
+++ client/src/index.js
@@ -4,40 +4,110 @@ import './index.css';
 
 
 const Square = (props) => {
-
-}
-
-
-class Grid extends React.Component {
-    render() {
-        return
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return
-    }
-}
-
-
-const Counter = (props) => {
-    const [count, setCount] = useState(0)
-
     return (
-        <div>
-            <p>Hello, {props.name}</p>
-            <p>Counter is {count}</p>
-            <button onClick={() => setCount(count + 1)}>
-              Increment
-            </button>
-        </div>
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
     );
 }
 
 
+class Row extends React.Component {
+    renderSquare(i) {
+        const value = this.props.squares[i]
+
+        return (
+            <Square
+                value={value === 0 ? "" : value.toString()}
+                onClick={() => this.props.onClick(this.props.rowNum, i)}
+                key={i.toString()}
+            />
+        );
+    }
+
+    render() {
+        const indexes = [...Array(9).keys()];
+
+        return (
+            <div className="grid-row">
+                {indexes.map((number) => 
+                    this.renderSquare(number)
+                )}
+            </div>
+        );
+    }
+}
+
+
+class Grid extends React.Component {
+    renderRow(row) {
+        const gameArray = this.props.gameArray;
+
+        return (
+            <Row
+                squares={gameArray[row]}
+                onClick={(y, x) => this.props.onClick(y, x)}
+                rowNum={row}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderRow(0)}
+                {this.renderRow(1)}
+                {this.renderRow(2)}
+                {this.renderRow(3)}
+                {this.renderRow(4)}
+                {this.renderRow(5)}
+                {this.renderRow(6)}
+                {this.renderRow(7)}
+                {this.renderRow(8)}
+            </div>
+        );
+    }
+}
+
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gameArray: [[0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5],
+                        [7,0,1,3,0,4,6,3,5],
+                        [0,0,1,3,0,4,6,3,5]],
+        };
+    }
+
+    handleClick(y, x) {
+        const grid = this.state.gameArray;
+        grid[y][x] = 1;
+        
+        this.setState({gameArray: grid});
+    }
+
+    render() {
+        return (
+            <div className="game-grid">
+                <Grid
+                    gameArray={this.state.gameArray}
+                    onClick={(y, x) => this.handleClick(y, x)}
+                />    
+            </div>
+        );
+    }
+}
+
+
 ReactDOM.render(
-    <Counter name="Maaz" />,
+    <Game />,
     document.getElementById('root')
 );
 
