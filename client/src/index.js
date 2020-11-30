4a9758fccf093f66463c33f0299099ac0a80f71f
maaz.hassan <h.maaz809@hotmail.com>
2020-11-30 06:59:59 -0700

    Responsive cell switching and blurring

--- client/src/index.js
+++ client/src/index.js
@@ -3,95 +3,96 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Square = (props) => {
+const Square = props => {
     return (
-        <button className="square" onClick={props.onClick} onContextMenu={props.onContextMenu}>
+        <button className={props.className} onClick={props.onClick} onContextMenu={props.onContextMenu}>
             {props.value}
         </button>
     );
 }
 
 
-class Row extends React.Component {
-    renderSquare(i) {
-        const value = this.props.squares[i]
+const Row = props => {
+    const renderSquare = i => {
+        const value = props.squares[i]
+        const [selY, selX] = props.selected;
+        const rowNum = props.rowNum;
 
         return (
             <Square
                 value={value === 0 ? "" : value.toString()}
-                onClick={() => this.props.onClick(this.props.rowNum, i)}
-                onContextMenu={(e) => this.props.onContextMenu(e, this.props.rowNum, i)}
+                onClick={() => props.onClick(rowNum, i)}
+                onContextMenu={(e) => props.onContextMenu(e, rowNum, i)}
                 key={i.toString()}
+                className={(i === selX && rowNum === selY) ? "square-selected" : "square"}
             />
         );
-    }
+    };
 
-    render() {
-        const indexes1 = [0, 1, 2];
-        const indexes2 = [3, 4, 5];
-        const indexes3 = [6, 7, 8];
+    const indexes1 = [0, 1, 2];
+    const indexes2 = [3, 4, 5];
+    const indexes3 = [6, 7, 8];
 
-        return (
-            <div className={this.props.rowClassName}>
-                <div className="row-piece">
-                    {indexes1.map((number) => 
-                        this.renderSquare(number)
-                    )}
-                </div>
-                <div className="row-piece">
-                    {indexes2.map((number) => 
-                        this.renderSquare(number)
-                    )}
-                </div>
-                <div className="row-piece">
-                    {indexes3.map((number) => 
-                        this.renderSquare(number)
-                    )}
-                </div>
+    return (
+        <div className={props.rowClassName}>
+            <div className="row-piece">
+                {indexes1.map((number) => 
+                    renderSquare(number)
+                )}
             </div>
-        );
-    }
+            <div className="row-piece">
+                {indexes2.map((number) => 
+                    renderSquare(number)
+                )}
+            </div>
+            <div className="row-piece">
+                {indexes3.map((number) => 
+                    renderSquare(number)
+                )}
+            </div>
+        </div>
+    );
 }
 
 
-class Grid extends React.Component {
-    renderRow(row) {
-        const gameArray = this.props.gameArray;
+const Grid = React.forwardRef((props, ref) => {
+    const renderRow = row => {
+        const gameArray = props.gameArray;
 
         return (
             <Row
                 squares={gameArray[row]}
-                onClick={(y, x) => this.props.onClick(y, x)}
-                onContextMenu={(e, y, x) => this.props.onContextMenu(e, y, x)}
+                onClick={(y, x) => props.onClick(y, x)}
+                onContextMenu={(e, y, x) => props.onContextMenu(e, y, x)}
                 rowNum={row}
-                rowClassName={(row == 2 || row == 5) ? "grid-row-3" : "grid-row"}
+                rowClassName={(row === 2 || row === 5) ? "grid-row-3" : "grid-row"}
+                selected={props.selected}
             />
         );
-    }
+    };
 
-    render() {
-        return (
-            <div className="game-grid">
-                {this.renderRow(0)}
-                {this.renderRow(1)}
-                {this.renderRow(2)}
-                {this.renderRow(3)}
-                {this.renderRow(4)}
-                {this.renderRow(5)}
-                {this.renderRow(6)}
-                {this.renderRow(7)}
-                {this.renderRow(8)}
-            </div>
-        );
-    }
-}
+    return (
+        <div className="game-grid" onBlur={(e) => props.onClickOutside(e)} ref={ref}>
+            {renderRow(0)}
+            {renderRow(1)}
+            {renderRow(2)}
+            {renderRow(3)}
+            {renderRow(4)}
+            {renderRow(5)}
+            {renderRow(6)}
+            {renderRow(7)}
+            {renderRow(8)}
+        </div>
+    );
+});
 
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        this.ref = React.createRef();
         this.state = {
-            gameArray: [[0,0,1,3,0,0,0,0,0],
+            gameArray: [[3,0,1,3,0,0,0,0,0],
                         [0,0,1,3,0,4,6,3,5],
                         [0,0,1,3,8,0,0,3,5],
                         [0,0,1,3,0,4,0,3,5],
@@ -100,14 +101,12 @@ class Game extends React.Component {
                         [0,0,1,3,0,4,6,2,5],
                         [7,0,1,3,0,4,6,3,5],
                         [0,0,1,3,0,4,6,3,5]],
+            selected: [null, null],
         };
     }
 
     handleClick(y, x) {
-        const grid = this.state.gameArray;
-        grid[y][x] = 5;
-        
-        this.setState({gameArray: grid});
+        this.setState({selected: [y, x]});
     }
 
     handleRightClick(e, y, x) {
@@ -116,7 +115,16 @@ class Game extends React.Component {
         const grid = this.state.gameArray;
         grid[y][x] = 0;
 
-        this.setState({gameArray: grid});
+        this.setState({
+            gameArray: grid,
+            selected: [y,x],
+        });
+    }
+
+    handleClickOutside(e) {
+        if (!this.ref.current.contains(e.relatedTarget)) {
+            this.setState({selected: [null, null]});
+        }
     }
 
     render() {
@@ -126,6 +134,9 @@ class Game extends React.Component {
                     gameArray={this.state.gameArray}
                     onClick={(y, x) => this.handleClick(y, x)}
                     onContextMenu={(e, y, x) => this.handleRightClick(e, y, x)}
+                    selected={this.state.selected}
+                    onClickOutside={(e) => this.handleClickOutside(e)}
+                    ref={this.ref}
                 />    
             </div>
         );
