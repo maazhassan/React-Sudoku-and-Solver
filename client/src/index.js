e9c0d8be3ba8143825df58f846488c47896983cb
maaz.hassan <h.maaz809@hotmail.com>
2020-12-01 19:10:49 -0700

    Algorithm to detect solved grid

--- client/src/index.js
+++ client/src/index.js
@@ -99,23 +99,24 @@ class Game extends React.Component {
         super(props);
         this.ref = React.createRef();
         this.state = {
-            gameArray: [[3,0,1,3,0,0,0,0,0],
-                        [0,0,1,3,0,4,6,3,5],
-                        [0,0,1,3,8,0,0,3,5],
-                        [0,0,1,3,0,4,0,3,5],
-                        [2,0,1,3,0,4,6,0,5],
-                        [0,0,1,3,0,4,9,3,5],
-                        [0,0,1,3,0,4,6,2,5],
-                        [7,0,1,3,0,4,6,3,5],
-                        [0,0,1,3,0,4,6,3,5]],
+            gameArray: [[5,3,4,6,7,8,9,1,2],
+                        [6,7,2,1,9,5,3,4,8],
+                        [1,9,8,3,4,2,5,6,7],
+                        [8,5,9,7,6,1,4,2,3],
+                        [4,2,6,8,5,3,7,9,1],
+                        [7,1,3,9,2,4,8,5,6],
+                        [9,6,1,5,3,7,2,8,4],
+                        [2,8,7,4,1,9,6,3,5],
+                        [3,4,5,2,8,6,1,7,0]],
             selected: [null, null],
+            solved: false,
         };
     }
 
     handleClick(e, y, x) {
         this.setState({selected: [y, x]});
 
-        if (e.button === 2) {
+        if (e.button === 2 && !this.state.solved) {
             const grid = this.state.gameArray;
             grid[y][x] = 0;
             this.setState({gameArray: grid});
@@ -130,19 +131,24 @@ class Game extends React.Component {
 
     handleKeyDown(e) {
         const selected = this.state.selected;
-        if (selected !== [null, null]) {
-            const allowed = ['1','2','3','4','5','6','7','8','9','Backspace']
+        if (selected !== [null, null] && !this.state.solved) {
+            const allowed = ['0','1','2','3','4','5','6','7','8','9','Backspace']
             const key = e.key;
             if (allowed.includes(key)) {
                 const [y, x] = selected;
                 const grid = this.state.gameArray;
-                grid[y][x] = key === 'Backspace' ? 0 : parseInt(key);
+                grid[y][x] = (key === 'Backspace') || key === '0' ? 0 : parseInt(key);
                 this.setState({gameArray: grid});
+                if (checkWin(grid)) {
+                    this.setState({solved: true});
+                }
             }
         }
     }
 
     render() {
+        const solved = this.state.solved;
+
         return (
             <div className="game-container">
                 <Grid
@@ -152,7 +158,8 @@ class Game extends React.Component {
                     onClickOutside={(e) => this.handleClickOutside(e)}
                     ref={this.ref}
                     onKeyDown={(e) => this.handleKeyDown(e)}
-                />    
+                />
+                <p>{solved ? "Solved" : "Not solved"}</p>
             </div>
         );
     }
@@ -165,3 +172,38 @@ ReactDOM.render(
 );
 
 
+function checkWin(board) {
+    for (let i = 0; i < board.length; i++) {
+        // Rows
+        if ((board[i].includes(0)) || (new Set(board[i]).size !== board[i].length)) {
+            return false;
+        }
+
+        // Columns
+        let col = [];
+        for (let j = 0; j < board.length; j++) {
+            col.push(board[j][i])
+        }
+        if ((col.includes(0)) || (new Set(col).size !== col.length)) {
+            return false;
+        }
+
+        // Squares
+        let square = [];
+        const startX = (i * 3) % 9;
+        const startY = Math.floor(i / 3) * 3
+        square.push(board[startY][startX])
+        square.push(board[startY][startX+1])
+        square.push(board[startY][startX+2])
+        square.push(board[startY+1][startX])
+        square.push(board[startY+1][startX+1])
+        square.push(board[startY+1][startX+2])
+        square.push(board[startY+2][startX])
+        square.push(board[startY+2][startX+1])
+        square.push(board[startY+2][startX+2])
+        if ((square.includes(0)) || (new Set(square).size !== square.length)) {
+            return false;
+        }
+    }
+    return true;
+}
